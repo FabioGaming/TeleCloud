5b0c0f0f420c98d818742b26ff1bfc19bc57af80
Fabio Gaming <fabio.furch@gmail.com>
2025-11-30 02:27:22 +0100

    Add response retry interceptor to HttpClient
    Introduces a response interceptor that retries failed requests once if they have not already been retried. This helps improve resilience against transient errors in HTTP requests.

--- src/shared/api/http-client.ts
+++ src/shared/api/http-client.ts
@@ -9,5 +9,16 @@ class HttpClient {
          baseURL: baseURL,
          timeout: 20000,
       });
+
+      this.instance.interceptors.response.use(
+         (res) => res,
+         async (error) => {
+            if (!error.config._retry) {
+               error.config._retry = true;
+               return this.instance(error.config);
+            }
+            return Promise.reject(error);
+         },
+      );
    }
 }
