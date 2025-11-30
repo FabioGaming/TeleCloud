f229512fcdaa1ef6b3690fe19bd261af62256412
FabioGaming <fabio.furch@gmail.com>
2025-11-30 07:50:57 +0100

    Rename PrivateChat interface to PrivateChatDto
    Renamed the PrivateChat interface and its file to PrivateChatDto for improved clarity and consistency in DTO naming conventions.

--- src/features/telegram/model/dto/chat/private-chat-dto.ts
+++ src/features/telegram/model/dto/chat/private-chat-dto.ts
@@ -1,6 +1,6 @@
 import type { TelegramChatBase } from "../../../../../shared/interfaces/telegram-chat-base";
 
-export interface PrivateChat extends TelegramChatBase {
+export interface PrivateChatDto extends TelegramChatBase {
    type: "private";
    first_name?: string;
    last_name?: string;
